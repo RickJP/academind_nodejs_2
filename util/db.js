0f74d62adb9db0117be44514d0f16e720ad26d38
Rick <3741326+RickJP@users.noreply.github.com>
2018-11-08 22:07:40 +0900

    Added function to connect to mongoDB; Removed sequelize code from app.js

--- util/db.js
+++ util/db.js
@@ -1,8 +1,21 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('academind_nodejs', 'root', 'apache2k', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://apache:WZzCoSrDQiNVUQIg@cluster0-yo2fk.mongodb.net/test?retryWrites=true'
+  )
+    .then(client => {
+      console.log('Connected');
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+    }); 
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
+
+
+
+
+
